3e465d60a060023d12c87cd8025edea769ad4b1b
Lasitha Siriweera <lasitha.siriweera@gmail.com>
2019-06-23 14:50:38 +0530

    search move api functions

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const cors = require("cors");
+const Movie = require('./movies');
 
+const movieModule = new Movie();
 
-app.get('/',async (req, res) => {
-    return res.send('Working');
+app.use(cors());
+
+app.get('/api/search', async (req, res) => {
+    try {
+        const keyWord = req.query.keyWord;
+        const movies = await movieModule.searchMovies(keyWord);
+        return res.json(movies);
+    } catch (error) {
+        return res.json({});
+    }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
---
+++ backend/movies.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+function movieModule() {
+    
+    /**
+     * search movies
+     * @param keyWord 
+     */
+    this.searchMovies = async (keyWord) => {
+        try {
+            const page1 = await getMoviesByName(keyWord, 1);
+            const page2 = await getMoviesByName(keyWord, 2);
+            const movies = page1.concat(page2);
+            return movies;
+        } catch (error) {
+            return [];
+        }
+    }
+
+    /**
+     * get movies using key word
+     * @param name 
+     * @param page 
+     */
+    getMoviesByName = async (keyWord, page) => {
+        try {
+            let data = [];
+            const dataSnap = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=83f14c69&s=' + keyWord + '&page' + page);
+            if (dataSnap && dataSnap.data && dataSnap.data.Search) {
+                data = dataSnap.data.Search;
+            }
+            return data;
+        } catch (error) {
+            return [];
+        }
+    }
+
+}
+
+module.exports = movieModule;
